a0b7e424e25fe45b9f1a87046f9e9f91903da83c
Emilio <emilio.lando@gmail.com>
2016-11-16 18:36:37 +0100

    safety

---
+++ Assets/Scripts/PlayerIconScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIconScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Input.GetTouch(0).position;
+        mousePos.z = 10f; 
+        this.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+}
