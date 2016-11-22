2046e1f03d7d8a907ca6eafc91755b35d690fe9f
Nimber <henrik.sve@hotmail.com>
2016-11-22 16:49:04 +0100

    Added main menu v.01

---
+++ Assets/Scripts/RestartScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class RestartScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject btn = GameObject.Find("Restart_button");
+        Button restart = btn.GetComponent<Button>();
+        restart.onClick.AddListener(changeToScene);
+	}
+
+    // Update is called once per frame
+    public void changeToScene()
+    {
+        Debug.Log("EOWEO?WPEOWPEOW");
+        SceneManager.LoadScene("main_menu");
+    }
+
+
+}
