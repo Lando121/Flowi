43e228f9a6c67ef0e90af1774e4a672b16c29f99
ferhixo <callvik@kth.se>
2016-11-30 16:51:40 +0100

    coinscript and so on

---
+++ Assets/Scripts/Collect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+            if(collider.name == "PlayerIcon")
+        {
+            Camera.main.GetComponent<GameLoop>().addScore(100);
+            Destroy(gameObject);
+        }   
+           
+        
+    }
+}
---
+++ Assets/Scripts/coinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinScript : MonoBehaviour
+{
+    public GameObject coin;
+
+    public void generateCoins(Vector3[] points, Line mainLine) {
+        int spawnChance = (int) mainLine.lineDifficulty * 10;
+        float width = mainLine.lineWidth * 0.15f;
+        
+
+        foreach (Vector3 p in points) {
+            Vector3 temp = p + mainLine.transform.position;
+            if (Random.Range(1, 100) <= spawnChance)
+            {
+               GameObject c =  Instantiate(coin, temp, Quaternion.identity);
+               c.transform.parent = mainLine.transform;
+                c.transform.localScale = new Vector3(width, width, width);
+                c.transform.localPosition = p + new Vector3(0, 0, -2);
+            }
+        }
+    }
+}
